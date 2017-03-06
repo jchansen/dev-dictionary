5650935b3e999344f2bb063af30e7e45eca8d7e3
Jason Hansen <jason_hansen@outlook.com>
2017-03-05 21:35:41 -0700

    update to lore v0.12.0-beta

--- config/env/development.js
+++ config/env/development.js
@@ -1,27 +1,113 @@
 /**
  * Development environment settings
  *
- * This file is where you define overrides for any of the config settings when operating under the development 
+ * This file is where you define overrides for any of the config settings when operating under the development
  * environment. Development environment is defined as `NODE_ENV=development` or the absence of an `NODE_ENV` environment
  * variable.
  **/
 
-// import { browserHistory } from 'react-router';
+var _ = require('lodash');
+var React = require('react');
+var Redux = require('redux');
+var ReduxDevTools = require('redux-devtools');
+var LogMonitor = require('redux-devtools-log-monitor').default;
+var DockMonitor = require('redux-devtools-dock-monitor').default;
+var Provider = require('react-redux').Provider;
+var Router = require('react-router').Router;
+var batchedSubscribe = require('redux-batched-subscribe').batchedSubscribe;
+
+/**
+ * Redux DevTools, for a practical and fun development experience
+ * https://github.com/gaearon/redux-devtools
+ */
+
+var DevTools = ReduxDevTools.createDevTools(
+  <DockMonitor
+    toggleVisibilityKey='ctrl-h'
+    changePositionKey='ctrl-q'
+    defaultIsVisible={true}>
+    <LogMonitor />
+  </DockMonitor>
+);
 
 export default {
 
-  /****************************************************************************
-  *                                                                           *
-  * This file will override other config settings based on the environment    *
-  *                                                                           *
-  ****************************************************************************/
+  /**
+   * Override Redux configuration in development to add support for the Redux DevTools
+   */
 
-  // models: {
-  //   apiRoot: 'https://api.example.dev'
-  // },
+  redux: {
 
-  // router: {
-  //   history: browserHistory
-  // }
+    /**
+     * Flag to enable/disable the Redux DevTools
+     */
+
+    devToolsEnabled: false,
+
+    /**
+     * If DevTools are enabled, enhance the Redux Store with instrumentation support
+     */
+
+    enhancer: function(middleware, config) {
+      if (config.redux.devToolsEnabled) {
+        return Redux.compose(
+          Redux.applyMiddleware.apply(null, middleware),
+          DevTools.instrument(),
+          batchedSubscribe(_.debounce(function(notify) {
+            console.log('notify()');
+            notify();
+          }, 0))
+        );
+      }
+
+      return Redux.compose(
+        Redux.applyMiddleware.apply(null, middleware),
+        batchedSubscribe(_.debounce(function(notify) {
+          console.log('notify()');
+          notify();
+        }, 0))
+      );
+    }
+
+  },
+
+  /**
+   * Override React configuration in development to add support for the Redux DevTools
+   */
+
+  react: {
+
+    /**
+     * If DevTools are enabled, we need to render them
+     */
+
+    getRootComponent: function(lore) {
+      var store = lore.store;
+      var routes = lore.router.routes;
+      var history = lore.router.history;
+      var devToolsEnabled = lore.config.redux.devToolsEnabled;
+
+      if (devToolsEnabled) {
+        return (
+          <Provider store={store}>
+            <div>
+              <Router history={history}>
+                {routes}
+              </Router>
+              <DevTools />
+            </div>
+          </Provider>
+        );
+      }
+
+      return (
+        <Provider store={store}>
+          <Router history={history}>
+            {routes}
+          </Router>
+        </Provider>
+      );
+    },
+  }
 
 }
