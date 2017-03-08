7d06198d0be807a904182261bb7a4c3cd88591f9
Jason Hansen <jason_hansen@outlook.com>
2017-03-08 02:03:16 -0700

    render errors in app

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap'
 import DefinitionAuthor from './DefinitionAuthor';
+import PayloadStates from '../constants/PayloadStates';
 
 class Definition extends Component {
   static propTypes = {
@@ -9,9 +11,23 @@ class Definition extends Component {
 
   render() {
     const { definition, index } = this.props;
+    let error = null;
+    let styles = {};
+
+    if (definition.state == PayloadStates.CREATING) {
+      styles.opacity = 0.3;
+    }
+
+    if (definition.state === PayloadStates.ERROR_CREATING) {
+      error = (
+        <Alert bsStyle="danger">
+          <strong>Uh oh!</strong> Something went wrong: {definition.error.message}
+        </Alert>
+      )
+    }
 
     return (
-      <div className="definition-container">
+      <div className="definition-container" style={styles}>
         <div className="definition">
           <div className="definition-index">
             {index}.
@@ -21,6 +37,7 @@ class Definition extends Component {
           </div>
         </div>
         <DefinitionAuthor definition={definition} />
+        {error}
       </div>
     );
   }
