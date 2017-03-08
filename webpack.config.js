4a5c123639fa8ad95a66987eecf5207cb494e5c3
Jason Hansen <jason_hansen@outlook.com>
2017-03-08 00:49:55 -0700

    convert to webpack v2

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,97 @@
-/**
- * This file builds and exports the final webpack config using the default
- * configuration in `/webpack/config.js` plus any environment specific
- * overrides that should be applied on top of it.
- *
- * This file also needs to specify the application root and pass it to the
- * webpack configs in `/webpack`, so `/webpack/config.js` can define the
- * `__LORE_ROOT__` constant (which the Lore framework uses to build file paths
- * to project folders like /models, /actions, /reducers, etc.
- *
- * Because webpack bundles the project into a single file under
- * `/dist/bundle.js`, it needs to know at *build* time what files it needs to
- * include.  So while dynamic require statements will work in a Node app
- * (because `require()` is a synchronous operation, that approach won't work
- * with Webpack. All file paths need to be explicit during build time.  The
- * alternative was to use relative paths to back out of the `lore` package in
- * `node_modules` and into the project itself, but that seemed like a worse
- * and somewhat fragile alternative.
- **/
+const {resolve} = require('path');
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const {getIfUtils, removeEmpty} = require('webpack-config-utils');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const requireDir = require('require-dir');
-const _ = require('lodash');
-const yargs = require('yargs');
-
-const envConfigs = requireDir('./webpack/env');
-
-const settings = {
-  APP_ROOT: __dirname,
-  PORT: yargs.argv.port || 3000
+module.exports = env => {
+  const {ifProd, ifNotProd} = getIfUtils(env);
+  const config = {
+    // context: resolve('src'),
+    entry: {
+      main: './index.js'
+    },
+    output: {
+      filename: ifProd('bundle.[name].[chunkhash].js', 'bundle.[name].js'),
+      path: resolve('dist'),
+      pathinfo: ifNotProd(),
+      // publicPath: '/assets/'
+    },
+    devtool: ifProd('source-map', 'eval'),
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
+          // include: APP_ROOT
+        },{
+          test: /\.css/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: 'css-loader'
+          })
+        },{
+          test: /\.less$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              'css-loader',
+              'less-loader'
+            ]
+          })
+        },{
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              'css-loader',
+              'sass-loader'
+            ]
+          })
+        },{
+          test: /\.(png|jpg|ttf|woff|woff2|eot)$/,
+          use: 'url-loader?limit=8192'
+        },
+        {
+          test: /\.svg$/,
+          use: 'svg-loader'
+        },{
+          test: /\.json/,
+          use: 'json-loader'
+        }]
+    },
+    plugins: removeEmpty([
+      new webpack.DefinePlugin({
+        __LORE_ROOT__: JSON.stringify(__dirname)
+      }),
+      new ProgressBarPlugin(),
+      new ExtractTextPlugin(ifProd(
+        'styles.[name].[chunkhash].css',
+        'styles.[name].css'
+      )),
+      ifProd(new ManifestPlugin({
+        fileName: 'asset-manifest.json'
+      })),
+      // ifProd(new webpack.optimize.CommonsChunkPlugin({
+      //   names: ['manifest'],
+      // })),
+      ifProd(new CopyWebpackPlugin([{
+        from: 'assets/images',
+        to: 'assets/images'
+      }])),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: 'body',
+      }),
+    ]),
+  };
+  if (env.debug) {
+    console.log(config);
+    debugger;
+  }
+  return config;
 };
-
-// build the base webpack config
-const config = require('./webpack/config')(settings);
-
-// get the environment specific config
-const env = process.env.NODE_ENV || 'development';
-const envConfig = envConfigs[env];
-
-// if there is a config for this environment, override the base
-// config with whatever is specified there
-if (envConfig) {
-  _.assign(config, envConfig(settings));
-}
-
-// export the final config file
-//
-// NOTE: module.exports is used instead of "export default" because Node doesn't understand
-// import/export yet, and using "export default" will break publishing tasks like "gulp surge".
-
-module.exports = config;
